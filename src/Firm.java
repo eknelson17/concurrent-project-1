44c681aecaf7fe5a1efc29cb69a53e49a2a665f1
Emma Nelson <eknelson17@gmail.com>
2013-01-03 17:44:30 -0500

    Adding partial class diagram and removed ConferenceRoom class. Replaced with an object in Firm.

--- src/Firm.java
+++ src/Firm.java
@@ -1,5 +1,10 @@
 
 public class Firm {
+    
+    /**
+     * Conference room lock - no need for a separate class.
+     */
+    public static final Object conferenceRoom = new Object();
 
 	public static void main(String[] args){
 		
