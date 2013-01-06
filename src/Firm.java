6d034c5cd31587325f8429c80d3f88ed894b2958
Curtis Burtner <cburtner007@gmail.com>
2013-01-05 22:17:15 -0500

    Added class constants/methods.
    MAJOR CONCERNS:
    
    -Initialization of the 2D Employee Array also starts each thread as it's
    created. Not sure if this is necessary.
    -Not sure when FirmTimer timer should be started. As of right now it is
    likely to be started after all the Employees are released from their
    threads. Need feedback on this.

--- src/Firm.java
+++ src/Firm.java
@@ -1,12 +1,126 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 public class Firm {
-    
-    /**
-     * Conference room lock - no need for a separate class.
-     */
-    public static final Object conferenceRoom = new Object();
-
-	public static void main(String[] args){
+	
+	/**
+	 * System constants
+	 */
+	private static final int NUMBER_OF_EMPLOYEES = 13; //3 teams + ProjectManager
+	private static final int NUMBER_OF_TEAMS = 3 ;
+	private static final int MEMBERS_PER_TEAM = 4 ;
+	
+	/**
+	 * This latch will be passed to each Employee (And the ProjectManager) in order to 
+	 * start all Employee threads at the same time
+	 */
+	private static final CountDownLatch cd = new CountDownLatch(NUMBER_OF_EMPLOYEES) ;
+	
+	/**
+	 * Conference room that Team leads must obtain a lock on before hosting meeting
+	 */
+	private final static Object confRoom = new Object();
+	
+	/**
+	 * The ProjectManager
+	 */
+	private final static ProjectManager pm = new ProjectManager(cd) ;
+	
+	/**
+	 * A random Random class constant. 
+	 */
+	private final static Random r = new Random() ;
+	
+	/**
+	 * The list of teams is represented by a 2d Array. Each array of employees represents
+	 * a single team, and the array of arrays represents all the teams in this firm. 
+	 */
+	private final static Employee[][] teams = Firm.populateTeams(NUMBER_OF_TEAMS, MEMBERS_PER_TEAM);
+	
+	/**
+	 * The timer that each thread will use to keep track of time
+	 */
+	private final static FirmTime timer = new FirmTime();
+	
+	/**
+	 * initializes a 2D array of Employees. Also starts each thread as it's created
+	 */
+	private static Employee[][] populateTeams(int numberOfTeams, int membersPerTeam ){
+		Employee[][] teams = new Employee[numberOfTeams][membersPerTeam] ;
 		
+		//Creates each team
+		for(int i = 0 ; i < numberOfTeams ; i++){
+			
+			//Creates team members (For each team)
+			for(int j = 0 ; j < membersPerTeam ; j++){
+				//The first member of a team will always be the team lead
+				if( j==0 ){
+					TeamLead tL = new TeamLead(j, i, cd) ;
+					teams[i][j] = tL;
+					tL.start() ;
+				}
+				//Everyone else is a regular employee
+				else{
+					Employee e = new Employee(j, i, cd) ;
+					teams[i][j] = e;
+					e.start() ;
+				}
+			}
+		}
+		return teams ;
+	}
+	
+	public static Employee[][] getAllEmployees(int id){
+		return teams ;	
+	}
+	
+	/**
+	 * Getter for confRoom
+	 * @return
+	 */
+	public static Object getConferenceRoom(){
+		return confRoom ;
+	}
+	
+	/**
+	 * Returns the leader of a passed Employee's team
+	 * @param the employee whose leader you want to find
+	 */
+	public static Employee getEmployeesLead(Employee e){
+		int teamID = e.getTeamID() ;
+		
+		Employee lead = teams[teamID][0] ;
+		
+		return lead ;
+	}
+	
+	/**
+	 * Getter for the firm's FirmTime
+	 * @return
+	 */
+	public static FirmTime getFirmTime(){
+		return timer ;
+	}
+	
+	/**
+	 * Getter for the firm's ProjectManager
+	 * @return
+	 */
+	public static ProjectManager getProjectManager(){
+		return pm ;
+	}
+	
+	/**
+	 * Getter for Random r
+	 * @return
+	 */
+	public static Random getRandom(){
+		return r ;
+	}
+	
+	public static void main(String[] args){
+		Firm.getFirmTime().start();
 	}
 }
